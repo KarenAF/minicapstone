fe932baea284951d4f8b6200c6306d7cedd11dc8
Karen <karenjxliu@hotmail.com>
2016-07-10 16:36:31 -0500

    Implement RESTful index

--- app/controllers/fruits_controller.rb
+++ app/controllers/fruits_controller.rb
@@ -3,6 +3,11 @@ class FruitsController < ApplicationController
     @fruits = Fruit.all
     render 'store.html.erb'
   end
+
+  def index
+    @fruits = Fruit.all
+    render 'index.html.erb'
+  end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get '/store' => 'fruits#store'
   get '/query_params_example' => 'params#query_params'
 
+  get '/fruits' => 'fruits#index'
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
