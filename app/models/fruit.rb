776de5724c5f32cc6466d30bb0c7a8fc815c98bd
Karen <karenjxliu@hotmail.com>
2016-07-24 11:59:08 -0500

    changed show page so that it will not show categories if the fruit has none.

--- app/models/fruit.rb
+++ app/models/fruit.rb
@@ -1,7 +1,6 @@
 # 7//13/16 7:33pm We've been using this the whole time(Rails used it automatically), but we're going to add more methods/add logic in here to help us with ruby code that we need. ("Business logic: move it out of your view and into your model.") Controller is kind of dumb in the sense that all it does is receive requests and send them around (like it's a switchboard.)
 class Fruit < ActiveRecord::Base
   belongs_to :supplier
-  belongs_to :user
   has_many :images
   has_many :orders
   has_many :categorized_fruits
