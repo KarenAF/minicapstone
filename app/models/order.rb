0cffc0170dc21a0aad2869107582b7b99bdc6f37
karenAF <karenjxliu@hotmail.com>
2017-07-29 17:16:20 -0500

    fixed orders creation by changing model validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,13 +4,14 @@ class Order < ActiveRecord::Base
   has_many :fruits, through: :carted_fruits
 
   validates :user_id, presence: true
-  validates :subtotal, numericality: {greater_than: 0, only_integer: true}
-  validates :total, numericality: {greater_than: 0, only_integer: true}
-  validates :tax, numericality: {greater_than: 0, only_integer: true}
+  validates :subtotal, numericality: {greater_than: 0}
+  validates :total, numericality: {greater_than: 0}
+  validates :tax, numericality: {greater_than: 0}
 
   def calculate_totals(input_carted_fruits)
     subtotal = 0
     input_carted_fruits.each do |carted_fruits|
+      subtotal += carted_fruits.quantity * carted_fruits.fruit.price
     end
   end
   # def tax
